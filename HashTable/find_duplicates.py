1bc077e8e6832007251dc932e3500a7b95d951c9
yutounun <geek.yuto@gmail.com>
2024-01-05 09:56:31 +0900

    find_duplicates

---
+++ HashTable/find_duplicates.py
@@ -0,0 +1,33 @@
+def find_duplicates(nums):
+    rtn = []
+    map = {}
+
+    for num in nums:
+        if num in map and num not in rtn:
+            rtn.append(num)
+        else:
+            map[num] = True
+    return rtn
+
+
+print(find_duplicates([1, 2, 3, 4, 5]))
+print(find_duplicates([1, 1, 2, 2, 3]))
+print(find_duplicates([1, 1, 1, 1, 1]))
+print(find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]))
+print(find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]))
+print(find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]))
+print(find_duplicates([]))
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    []
+    [1, 2]
+    [1]
+    [3, 4]
+    [1, 2, 3]
+    [1, 2, 3]
+    []
+
+"""
