c03d81b0648c84f3baac6a7b0eb6ea5fe7feb2df
yutounun <geek.yuto@gmail.com>
2024-01-05 10:03:27 +0900

    add 2nd solution to find_duplicates

--- HashTable/find_duplicates.py
+++ HashTable/find_duplicates.py
@@ -1,13 +1,28 @@
 def find_duplicates(nums):
-    rtn = []
-    map = {}
+    # Solution1
+    # rtn = []
+    # map = {}
+
+    # for num in nums:
+    #     if num in map and num not in rtn:
+    #         rtn.append(num)
+    #     else:
+    #         map[num] = True
+    # return rtn
+
+    # Solution2
+    num_counts = {}
 
     for num in nums:
-        if num in map and num not in rtn:
-            rtn.append(num)
-        else:
-            map[num] = True
-    return rtn
+        # {num: count}
+        num_counts[num] = num_counts.get(num, 0) + 1
+    # Include keys having a value of more than 1
+    # duplicates = [num for num, count in num_counts.items() if count > 1]
+    duplicates = []
+    for key, count in num_counts.items():
+        if count > 1:
+            duplicates.append(key)
+    return duplicates
 
 
 print(find_duplicates([1, 2, 3, 4, 5]))
