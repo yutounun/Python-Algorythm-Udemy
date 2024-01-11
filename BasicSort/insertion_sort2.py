1f70a1483038c20d02af212a4b8f66e203a48163
yutounun <geek.yuto@gmail.com>
2024-01-11 09:23:58 +0900

    insertion_sort2

---
+++ BasicSort/insertion_sort2.py
@@ -0,0 +1,86 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def insertion_sort(self):
+        if self.length < 2:
+            return
+        
+        current = self.head.next
+        while current:
+            left = self.head
+            while left != current:
+                if left.value > current.value:
+                   current.value, left.value = left.value, current.value
+                left = left.next
+            current = current.next
+                
+        
+
+
+
+
+
+my_linked_list = LinkedList(4)
+my_linked_list.append(2)
+my_linked_list.append(6)
+my_linked_list.append(5)
+my_linked_list.append(1)
+my_linked_list.append(3)
+
+print("Linked List Before Sort:")
+my_linked_list.print_list()
+
+my_linked_list.insertion_sort()
+
+print("\nSorted Linked List:")
+my_linked_list.print_list()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Linked List Before Sort:
+    4
+    2
+    6
+    5
+    1
+    3
+
+    Sorted Linked List:
+    1
+    2
+    3
+    4
+    5
+    6
+
+"""
+
