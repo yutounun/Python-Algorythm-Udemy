638ef210fd69d91222c3e478b4c0df13280b6619
yutounun <geek.yuto@gmail.com>
2024-01-13 18:21:08 +0900

    max_profit

---
+++ Other/max_profit.py
@@ -0,0 +1,53 @@
+def max_profit(prices):
+    max_profit = 0
+    current = 0
+    min_val = prices[0]
+
+    while current < len(prices):
+        min_val = min(min_val, prices[current])
+        max_profit = max(prices[current] - min_val, max_profit)
+        current += 1
+    return max_profit
+
+
+prices = [7, 1, 5, 3, 6, 4]
+profit = max_profit(prices)
+print("Test with mixed prices:")
+print("Prices:", prices)
+print("Maximum profit:", profit)
+print("-----------------------------")
+
+
+prices = [7, 6, 4, 3, 1]
+profit = max_profit(prices)
+print("Test with descending prices:")
+print("Prices:", prices)
+print("Maximum profit:", profit)
+print("-----------------------------")
+
+
+prices = [1, 2, 3, 4, 5, 6]
+profit = max_profit(prices)
+print("Test with ascending prices:")
+print("Prices:", prices)
+print("Maximum profit:", profit)
+print("-----------------------------")
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Test with mixed prices:
+    Prices: [7, 1, 5, 3, 6, 4]
+    Maximum profit: 5
+    -----------------------------
+    Test with descending prices:
+    Prices: [7, 6, 4, 3, 1]
+    Maximum profit: 0
+    -----------------------------
+    Test with ascending prices:
+    Prices: [1, 2, 3, 4, 5, 6]
+    Maximum profit: 5
+    -----------------------------
+
+"""
